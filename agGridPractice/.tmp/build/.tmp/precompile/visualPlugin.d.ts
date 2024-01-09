38c28e747baed98c83461da140fca89d5f6ec0d9
Ajaikumar-N <ajainatarajan6@gmail.com>
2024-01-09 17:24:57 +0530

    ag grid practice

---
+++ agGridPractice/.tmp/build/.tmp/precompile/visualPlugin.d.ts
@@ -0,0 +1,4 @@
+import powerbiVisualsApi from "powerbi-visuals-api";
+import IVisualPlugin = powerbiVisualsApi.visuals.plugins.IVisualPlugin;
+declare var agGridPractice67F04495CEFA4FB3836950577593BF54_DEBUG: IVisualPlugin;
+export default agGridPractice67F04495CEFA4FB3836950577593BF54_DEBUG;
---
+++ agGridPractice/.tmp/build/src/Table.d.ts
@@ -0,0 +1,8 @@
+import * as React from "react";
+import "ag-grid-community/styles/ag-grid.css";
+import "ag-grid-community/styles/ag-theme-quartz.css";
+declare const TableVisual: ({ row, col }: {
+    row: any;
+    col: any;
+}) => React.JSX.Element;
+export default TableVisual;

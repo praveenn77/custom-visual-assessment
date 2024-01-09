a86e0713290fe58bfc419053b83f7720bbcb8dca
karthik p <108673648+karthik0410@users.noreply.github.com>
2024-01-09 17:34:59 +0530

    Custom Visual Files Added

---
+++ toolTip/.tmp/build/.tmp/precompile/visualPlugin.d.ts
@@ -0,0 +1,4 @@
+import powerbiVisualsApi from "powerbi-visuals-api";
+import IVisualPlugin = powerbiVisualsApi.visuals.plugins.IVisualPlugin;
+declare var toolTipC4124734287E4410BB2C649C3D901EF6_DEBUG: IVisualPlugin;
+export default toolTipC4124734287E4410BB2C649C3D901EF6_DEBUG;
